0de4ada25ad14f2b4afa32483a37f39da0e7b20c
3cognito <tommyubongabasijohn@gmail.com>
2025-03-20 03:06:21 +0100

    user model with validation

---
+++ src/models/user.model.ts
@@ -0,0 +1,40 @@
+import { IsEmail, MinLength } from "class-validator";
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column()
+  @MinLength(2, { message: "firstname must be a minimum of two characters" })
+  firstname!: string;
+
+  @Column()
+  lastname!: string;
+
+  @Column()
+  @Index()
+  @IsEmail({}, { message: "please enter a valid email" })
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+
+  @DeleteDateColumn()
+  deletedAt!: Date;
+}
