4883024a5d599d4d97cbfaf028d7401fadadc99e
3cognito <tommyubongabasijohn@gmail.com>
2025-03-24 07:58:51 +0100

    point and run model

---
+++ src/models/point.model.ts
@@ -0,0 +1,53 @@
+import {
+  Check,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from "typeorm";
+import { Run } from "./run.model";
+
+@Entity()
+@Unique(["run_id", "latitude", "longitude", "timestamp"])
+export class Point {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column({ type: "uuid" })
+  @Index()
+  @ManyToOne(() => Run)
+  @JoinColumn({ name: "run_id" })
+  run_id!: string;
+
+  @Column({ type: "float8" })
+  @Check(`latitude BETWEEN -90 AND 90`)
+  latitude!: number;
+
+  @Column({ type: "float8" })
+  @Check(`longitude BETWEEN -180 AND 180`)
+  longitude!: number;
+
+  @Column({ type: "float" })
+  @Check(`altitude >= 0`)
+  altitude!: number;
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  //TODO: some kind of validation to ensure newer entries cannot have older timestamps??
+  //Note to self: validation does not necessarily have to be done at model level???
+  timestamp!: Date;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+
+  @DeleteDateColumn()
+  deletedAt!: Date;
+}
---
+++ src/models/run.model.ts
@@ -0,0 +1,48 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+export enum RunStatus {
+  IN_PROGRESS = "in_progress",
+  COMPLETED = "completed",
+}
+
+@Entity()
+export class Run {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column()
+  @Index()
+  userID!: string;
+
+  @Column({ type: "enum", enum: RunStatus, default: RunStatus.IN_PROGRESS })
+  status!: string;
+
+  @Column({ nullable: true })
+  finishedAt!: Date;
+
+  @Column({ nullable: true })
+  totalDistance!: number; //in centimeters
+
+  @Column({ nullable: true })
+  timeElapsed!: number; // in seconds
+
+  @Column({ type: "float", nullable: true })
+  caloriesBurned!: number;
+
+  @CreateDateColumn()
+  createdAt!: Date; //using this as start time, under the assumption user starts running when they create a run
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+
+  @DeleteDateColumn()
+  deletedAt!: Date;
+}
