ea8cb5d3e689f08b5b3347c3053d10e09a195b4e
3cognito <tommyubongabasijohn@gmail.com>
2025-03-27 14:26:50 +0100

    update plan

--- src/services/stats.service.ts
+++ src/services/stats.service.ts
@@ -28,7 +28,6 @@ const haversineDistance = (
     Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon / 2) ** 2;
 
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  console.log(coord1, coord2);
   return R * c; // Distance in meters
 };
 
