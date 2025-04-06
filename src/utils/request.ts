969328a8635730b20ef7b92c92d4551b89bc1ee9
3cognito <tommyubongabasijohn@gmail.com>
2025-04-06 08:29:08 +0100

    extend request utility to support authorixation

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,12 +2,14 @@ export async function makeRequest<T>(
   method: "GET" | "POST" | "PUT" | "DELETE",
   url: string,
   body?: object,
+  token?: string,
   headers?: Record<string, string>
 ): Promise<Response> {
   const options: RequestInit = {
     method,
     headers: {
       "Content-Type": "application/json",
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
       ...(headers || {}),
     },
     body: body ? JSON.stringify(body) : undefined,
