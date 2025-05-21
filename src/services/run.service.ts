5904898ed634b6c97383075e01376fd3367fcc31
3cognito <tommyubongabasijohn@gmail.com>
2025-05-21 18:25:51 +0100

    test exclusion

--- src/services/run.service.ts
+++ src/services/run.service.ts
@@ -230,6 +230,14 @@ export class RunService {
     }
   }
 
+    private async addRuntoRedis(r: RunStat) {
+    try {
+      await this.redisClient.addObject(r.runId, r);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
       private async addRuntoRedis(r: RunStat) {
     try {
       await this.redisClient.addObject(r.runId, r);
